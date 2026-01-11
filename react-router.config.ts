59f9513f11baa735dd71350ff7b35cc6d98b3356
bodj <jonathanjaazer21@gmail.com>
2026-01-11 15:26:13 +0800

    404 not found fix in vercel

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,4 +2,6 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true, // Enable SSR for nodemailer email service
+  buildDirectory: "build",
+  serverBuildFile: "index.js",
 } satisfies Config;
